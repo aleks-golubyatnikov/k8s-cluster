8be70163bc408fc73be23ed851c28bbe94638460
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-02-22 22:30:24 +0300

    Change directory

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 !#/bin/sh
-cd /home/golubyatnikov/k8s-cluster/ansible-playbook
+cd /home/init-cluster/k8s-cluster/ansible-playbook
 ansible-playbook -i hosts kube-dependencies.yml -k
 ansible-playbook -i hosts master.yml -k
