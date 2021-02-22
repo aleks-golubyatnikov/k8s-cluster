943c77b633c69aab3858fa5495bd908572e85e0b
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-02-22 22:31:09 +0300

    Change '.\run.sh'

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 !#/bin/sh
 cd /home/init-cluster/k8s-cluster/ansible-playbook
-ansible-playbook -i hosts kube-dependencies.yml -k
-ansible-playbook -i hosts master.yml -k
+ansible-playbook -i hosts kube-dependencies.yml
+ansible-playbook -i hosts master.yml
