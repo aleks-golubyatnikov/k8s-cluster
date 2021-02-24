82550dd83fdb661c6e458cfc2073a9c33aece0dd
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-02-24 17:58:06 +0300

    Fix errors-2

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ ansible-playbook -i ./ansible-playbook/hosts ./ansible-playbook/initial.yml >> i
 ansible-playbook -i ./ansible-playbook/hosts ./ansible-playbook/kube-dependencies.yml >> instalation.log
 ansible-playbook -i ./ansible-playbook/hosts ./ansible-playbook/master.yml >> configure-master.log
 
+
 #ansible-playbook -i ./ansible-playbook/hosts ./ansible-playbook/workers.yml >> configure-workers.log
 #ansible-playbook -i ./ansible-playbook/hosts ./ansible-playbook/k8s-cluster.yml >> configure-k8s-cluster.log
