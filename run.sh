0307856ad521cba1022e1640f9dc5b20f8e6ddad
Aleks Golubyatnikov <aleks.golubyatnikov@gmail.com>
2021-02-24 15:53:33 +0300

    Edit .\run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 ansible-playbook -i ./ansible-playbook/hosts ./ansible-playbook/kube-dependencies.yml >> instalation.log
-#ansible-playbook -i init-cluster/ansible-playbook/hosts init-cluster/ansible-playbook/master.yml
+ansible-playbook -i ./ansible-playbook/hosts ./ansible-playbook/master.yml >> configure-master.log
+kubectl get nodes >> nodes.log
